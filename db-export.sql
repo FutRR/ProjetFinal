ee622e21faccd1ced80336e17c50fea77a8dafdb
FutRR <futterermaxime@gmail.com>
2024-07-02 14:03:04 +0200

    db export

--- db-export.sql
+++ db-export.sql
@@ -19,29 +19,40 @@
 CREATE DATABASE IF NOT EXISTS `projet_final_maximefutterer` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `projet_final_maximefutterer`;
 
+-- Listage de la structure de table projet_final_maximefutterer. doctrine_migration_versions
+CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+-- Listage des données de la table projet_final_maximefutterer.doctrine_migration_versions : ~1 rows (environ)
+INSERT IGNORE INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+	('DoctrineMigrations\\Version20240702114918', '2024-07-02 11:49:51', 41);
+
 -- Listage de la structure de table projet_final_maximefutterer. etape
 CREATE TABLE IF NOT EXISTS `etape` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nom_etape` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `pdf` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `video` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nom_etape` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pdf` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `video` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `ordre` int NOT NULL,
   `niveau_id` int NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `IDX_285F75DDB3E9C81` (`niveau_id`),
+  CONSTRAINT `FK_285F75DDB3E9C81` FOREIGN KEY (`niveau_id`) REFERENCES `niveau` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet_final_maximefutterer.etape : ~2 rows (environ)
-INSERT IGNORE INTO `etape` (`id`, `nom_etape`, `pdf`, `video`, `description`, `ordre`, `niveau_id`) VALUES
-	(1, 'Lecture', NULL, NULL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lacus lorem, molestie sit amet rutrum ut, porta nec ante. Suspendisse iaculis ullamcorper quam, nec iaculis tellus tincidunt convallis. Suspendisse et luctus enim, sed accumsan velit. Suspendisse non purus nec nisl lobortis fermentum nec sit amet nunc. Nullam faucibus convallis purus, at posuere sapien sollicitudin sit amet. Nam sed augue et diam facilisis laoreet. Aenean lorem augue, elementum et ligula vel, ultrices pellentesque mauris.', 1, 1),
-	(2, 'Rythmique', NULL, NULL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lacus lorem, molestie sit amet rutrum ut, porta nec ante. Suspendisse iaculis ullamcorper quam, nec iaculis tellus tincidunt convallis. Suspendisse et luctus enim, sed accumsan velit. Suspendisse non purus nec nisl lobortis fermentum nec sit amet nunc. Nullam faucibus convallis purus, at posuere sapien sollicitudin sit amet. Nam sed augue et diam facilisis laoreet. Aenean lorem augue, elementum et ligula vel, ultrices pellentesque mauris.', 2, 1);
+-- Listage des données de la table projet_final_maximefutterer.etape : ~0 rows (environ)
 
 -- Listage de la structure de table projet_final_maximefutterer. messenger_messages
 CREATE TABLE IF NOT EXISTS `messenger_messages` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `body` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
   `available_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
   `delivered_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)',
@@ -56,28 +67,30 @@ CREATE TABLE IF NOT EXISTS `messenger_messages` (
 -- Listage de la structure de table projet_final_maximefutterer. niveau
 CREATE TABLE IF NOT EXISTS `niveau` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nom_niveau` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` longtext COLLATE utf8mb4_unicode_ci,
+  `nom_niveau` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet_final_maximefutterer.niveau : ~0 rows (environ)
-INSERT IGNORE INTO `niveau` (`id`, `nom_niveau`, `description`) VALUES
-	(1, 'Niveau 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lacus lorem, molestie sit amet rutrum ut, porta nec ante. Suspendisse iaculis ullamcorper quam, nec iaculis tellus tincidunt convallis. Suspendisse et luctus enim, sed accumsan velit. Suspendisse non purus nec nisl lobortis fermentum nec sit amet nunc. Nullam faucibus convallis purus, at posuere sapien sollicitudin sit amet. Nam sed augue et diam facilisis laoreet. Aenean lorem augue, elementum et ligula vel, ultrices pellentesque mauris.');
+-- Listage des données de la table projet_final_maximefutterer.niveau : ~2 rows (environ)
+INSERT IGNORE INTO `niveau` (`id`, `nom_niveau`) VALUES
+	(1, 'Niveau 1'),
+	(2, 'Niveau 2');
 
 -- Listage de la structure de table projet_final_maximefutterer. utilisateur
 CREATE TABLE IF NOT EXISTS `utilisateur` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(180) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `roles` json NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `register_date` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table projet_final_maximefutterer.utilisateur : ~1 rows (environ)
-INSERT IGNORE INTO `utilisateur` (`id`, `email`, `roles`, `password`) VALUES
-	(1, 'futterermaxime@gmail.com', '[]', '$2y$13$2m0OgmrAHZyvSJ7e0A7m8e623cB4RmOhtQlROVH0DmBovNRjMqUiG');
+INSERT IGNORE INTO `utilisateur` (`id`, `email`, `roles`, `password`, `username`, `register_date`) VALUES
+	(1, 'futterermaxime@gmail.com', '[]', '$2y$13$2m0OgmrAHZyvSJ7e0A7m8e623cB4RmOhtQlROVH0DmBovNRjMqUiG', 'FutRR', '2024-07-02 11:39:12');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
