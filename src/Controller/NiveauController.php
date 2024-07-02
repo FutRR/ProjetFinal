d0518d4269cadf550c2c22c5464bfd3f24e0bcb8
FutRR <futterermaxime@gmail.com>
2024-07-02 09:28:12 +0200

    added add & edit form for level entity

--- src/Controller/NiveauController.php
+++ src/Controller/NiveauController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Niveau;
+use App\Form\NiveauType;
 use App\Repository\EtapeRepository;
 use App\Repository\NiveauRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,6 +21,37 @@ class NiveauController extends AbstractController
         return $this->render('niveau/index.html.twig', [
             'niveaux' => $niveaux,
         ]);
+    }
+
+    #[Route('/niveau/new', name: 'new_niveau')]
+    #[Route('/niveau/{id}/edit', name: 'edit_niveau')]
+    public function new_edit(Niveau $niveau = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $isNewNiveau = !$niveau;
+        $message = $isNewNiveau ? 'Niveau créé' : 'Niveau modifié';
+
+        if (!$niveau) {
+            $niveau = new Niveau();
+        }
+
+        $form = $this->createForm(NiveauType::class, $niveau);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $niveau = $form->getData();
+            $entityManager->persist($niveau);
+            $entityManager->flush();
+            $this->addFlash('success', $message);
+            return $this->redirectToRoute('app_niveau');
+        }
+
+        return $this->render("niveau/new.html.twig", [
+            'formAddNiveau' => $form,
+            'edit' => $niveau->getId()
+        ]);
+
     }
 
     #[Route('/niveau/{id}', name: 'show_niveau')]
---
+++ src/Form/NiveauType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Niveau;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class NiveauType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomNiveau', TextType::class, [
+                'label' => 'Nom du niveau',
+                'attr' => ['class' => 'form']
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description du niveau',
+                'attr' => ['class' => 'form']
+            ])
+            ->add('valider', SubmitType::class, [
+                "attr" => [
+                    'class' => "submit btn"
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Niveau::class,
+        ]);
+    }
+}
