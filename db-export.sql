0686aadeccfe6ec2a39e5eff264febd6f14bb944
FutRR <futterermaxime@gmail.com>
2024-07-03 16:18:03 +0200

    db export

--- db-export.sql
+++ db-export.sql
@@ -27,9 +27,10 @@ CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
   PRIMARY KEY (`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
--- Listage des données de la table projet_final_maximefutterer.doctrine_migration_versions : ~1 rows (environ)
+-- Listage des données de la table projet_final_maximefutterer.doctrine_migration_versions : ~0 rows (environ)
 INSERT IGNORE INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-	('DoctrineMigrations\\Version20240702114918', '2024-07-02 11:49:51', 41);
+	('DoctrineMigrations\\Version20240702114918', '2024-07-02 11:49:51', 41),
+	('DoctrineMigrations\\Version20240703122431', '2024-07-03 12:24:52', 123);
 
 -- Listage de la structure de table projet_final_maximefutterer. etape
 CREATE TABLE IF NOT EXISTS `etape` (
@@ -43,18 +44,18 @@ CREATE TABLE IF NOT EXISTS `etape` (
   PRIMARY KEY (`id`),
   KEY `IDX_285F75DDB3E9C81` (`niveau_id`),
   CONSTRAINT `FK_285F75DDB3E9C81` FOREIGN KEY (`niveau_id`) REFERENCES `niveau` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table projet_final_maximefutterer.etape : ~8 rows (environ)
 INSERT IGNORE INTO `etape` (`id`, `nom_etape`, `pdf`, `video`, `description`, `ordre`, `niveau_id`) VALUES
-	(1, 'laudantium', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 1, 1),
-	(2, 'et', NULL, NULL, 'lorem ipsum blabla', 2, 1),
-	(3, 'provident', NULL, NULL, 'lorem ipsum blabla', 3, 1),
-	(4, 'cupiditate', NULL, NULL, 'lorem ipsum blabla', 4, 1),
-	(5, 'ut', NULL, NULL, 'lorem ipsum blabla', 1, 2),
-	(6, 'eos', NULL, NULL, 'lorem ipsum blabla', 2, 2),
-	(7, 'aut', NULL, NULL, 'lorem ipsum blabla', 3, 2),
-	(8, 'assumenda', NULL, NULL, 'lorem ipsum blabla', 4, 2);
+	(1, 'Lecture', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 1, 1),
+	(2, 'Rythme', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 2, 1),
+	(3, 'Connaissances théoriques', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 3, 1),
+	(4, 'Morceau', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 4, 1),
+	(5, 'Lecture', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 1, 2),
+	(6, 'Rythme', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 2, 2),
+	(7, 'Connaissances théoriques', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 3, 2),
+	(8, 'Morceau', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 4, 2);
 
 -- Listage de la structure de table projet_final_maximefutterer. messenger_messages
 CREATE TABLE IF NOT EXISTS `messenger_messages` (
@@ -85,6 +86,23 @@ INSERT IGNORE INTO `niveau` (`id`, `nom_niveau`) VALUES
 	(1, 'Niveau 1'),
 	(2, 'Niveau 2');
 
+-- Listage de la structure de table projet_final_maximefutterer. progression
+CREATE TABLE IF NOT EXISTS `progression` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `utilisateur_id` int NOT NULL,
+  `etape_id` int NOT NULL,
+  `done` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `IDX_D5B25073FB88E14F` (`utilisateur_id`),
+  KEY `IDX_D5B250734A8CA2AD` (`etape_id`),
+  CONSTRAINT `FK_D5B250734A8CA2AD` FOREIGN KEY (`etape_id`) REFERENCES `etape` (`id`),
+  CONSTRAINT `FK_D5B25073FB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Listage des données de la table projet_final_maximefutterer.progression : ~0 rows (environ)
+INSERT IGNORE INTO `progression` (`id`, `utilisateur_id`, `etape_id`, `done`) VALUES
+	(1, 1, 1, 0);
+
 -- Listage de la structure de table projet_final_maximefutterer. utilisateur
 CREATE TABLE IF NOT EXISTS `utilisateur` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -97,9 +115,9 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet_final_maximefutterer.utilisateur : ~1 rows (environ)
+-- Listage des données de la table projet_final_maximefutterer.utilisateur : ~0 rows (environ)
 INSERT IGNORE INTO `utilisateur` (`id`, `email`, `roles`, `password`, `username`, `register_date`) VALUES
-	(1, 'futterermaxime@gmail.com', '[]', '$2y$13$2m0OgmrAHZyvSJ7e0A7m8e623cB4RmOhtQlROVH0DmBovNRjMqUiG', 'FutRR', '2024-07-02 11:39:12');
+	(1, 'futterermaxime@gmail.com', '[]', '$2y$13$2m0OgmrAHZyvSJ7e0A7m8e623cB4RmOhtQlROVH0DmBovNRjMqUiG', 'FutRR_', '2024-07-02 11:39:12');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
