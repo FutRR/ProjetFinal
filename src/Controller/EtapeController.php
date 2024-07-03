5a9d71ec96f7b02243403761950470359017b8ef
FutRR <futterermaxime@gmail.com>
2024-07-03 12:05:01 +0200

    added etape form and links towards previous and next steps/levels

--- src/Controller/EtapeController.php
+++ src/Controller/EtapeController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Etape;
+use App\Form\EtapeType;
 use App\Repository\EtapeRepository;
 use App\Repository\NiveauRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,6 +22,38 @@ class EtapeController extends AbstractController
             'etapes' => $etapes,
         ]);
     }
+
+    #[Route('/etape/new', name: 'new_etape')]
+    #[Route('/etape/{id}/edit', name: 'edit_etape')]
+    public function new_edit(Etape $etape = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $isNewEtape = !$etape;
+        $message = $isNewEtape ? 'Étape créé' : 'Étape modifié';
+
+        if (!$etape) {
+            $etape = new Etape();
+        }
+
+        $form = $this->createForm(EtapeType::class, $etape);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $etape = $form->getData();
+            $entityManager->persist($etape);
+            $entityManager->flush();
+            $this->addFlash('success', $message);
+            return $this->redirectToRoute('app_etape');
+        }
+
+        return $this->render("etape/new.html.twig", [
+            'formAddEtape' => $form,
+            'edit' => $etape->getId()
+        ]);
+
+    }
+
 
     #[Route('/etape/{id}', name: 'show_etape')]
     public function show(Etape $etape, EtapeRepository $etapeRepository): Response
---
+++ src/Form/EtapeType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Etape;
+use App\Entity\Niveau;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class EtapeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomEtape', TextType::class, [
+                'attr' => ['class' => 'form']
+            ])
+            ->add('pdf', TextType::class, [
+                'attr' => ['class' => 'form']
+            ])
+            ->add('video', TextType::class, [
+                'attr' => ['class' => 'form']
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => ['class' => 'form']
+            ])
+            // ->add('ordre', NumberType::class, [
+            //     'attr' => ['class' => 'form']
+            // ])
+            ->add('Niveau', EntityType::class, [
+                'class' => Niveau::class,
+                'choice_label' => 'nomNiveau',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Etape::class,
+        ]);
+    }
+}
