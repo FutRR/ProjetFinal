59243c029b61e2e4f1526a686c54662f39667e46
FutRR <futterermaxime@gmail.com>
2024-07-05 16:47:09 +0200

    db export & migrations

--- db-export.sql
+++ db-export.sql
@@ -19,25 +19,32 @@
 CREATE DATABASE IF NOT EXISTS `projet_final_maximefutterer` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `projet_final_maximefutterer`;
 
--- Listage de la structure de table projet_final_maximefutterer. doctrine_migration_versions
-CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
-  `version` varchar(191) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `executed_at` datetime DEFAULT NULL,
-  `execution_time` int DEFAULT NULL,
-  PRIMARY KEY (`version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+-- Listage de la structure de table projet_final_maximefutterer. avis
+CREATE TABLE IF NOT EXISTS `avis` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `utilisateur_id` int NOT NULL,
+  `contenu` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_creation` datetime NOT NULL,
+  `note` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_8F91ABF0FB88E14F` (`utilisateur_id`),
+  CONSTRAINT `FK_8F91ABF0FB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet_final_maximefutterer.doctrine_migration_versions : ~0 rows (environ)
-INSERT IGNORE INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-	('DoctrineMigrations\\Version20240702114918', '2024-07-02 11:49:51', 41),
-	('DoctrineMigrations\\Version20240703122431', '2024-07-03 12:24:52', 123);
+-- Listage des données de la table projet_final_maximefutterer.avis : ~1 rows (environ)
+INSERT IGNORE INTO `avis` (`id`, `utilisateur_id`, `contenu`, `date_creation`, `note`) VALUES
+	(1, 1, 'cool', '2024-07-05 14:20:48', 5),
+	(2, 1, 'test', '2024-07-05 14:30:40', 2),
+	(3, 2, 'exemple', '2024-07-05 14:33:41', 1),
+	(4, 2, 'test 2', '2024-07-05 14:38:28', 5),
+	(5, 2, 'test 3', '2024-07-05 14:41:31', 4);
 
 -- Listage de la structure de table projet_final_maximefutterer. etape
 CREATE TABLE IF NOT EXISTS `etape` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nom_etape` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `pdf` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `video` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `video` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `description` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `ordre` int NOT NULL,
   `niveau_id` int NOT NULL,
@@ -48,11 +55,11 @@ CREATE TABLE IF NOT EXISTS `etape` (
 
 -- Listage des données de la table projet_final_maximefutterer.etape : ~8 rows (environ)
 INSERT IGNORE INTO `etape` (`id`, `nom_etape`, `pdf`, `video`, `description`, `ordre`, `niveau_id`) VALUES
-	(1, 'Lecture', 'pdf/piano-facile.pdf', 'https://www.youtube.com/embed/n7RjlO-beJA', 'lorem ipsum blabla', 1, 1),
+	(1, 'Lecture test', 'pdf/Commandes-Symfony-6687b9c8a58dd.pdf', 'https://www.youtube.com/embed/n7lOJNYGYvw', 'lorem ipsum blabla', 1, 1),
 	(2, 'Rythme', 'pdf/piano-facile.pdf', 'https://www.youtube.com/embed/n7RjlO-beJA', 'lorem ipsum blabla', 2, 1),
-	(3, 'Connaissances théoriques', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 3, 1),
+	(3, 'Connaissances théoriques', 'pdf/Commandes-Symfony-6687b9c8a58dd.pdf', 'https://www.youtube.com/embed/YaGiM2cPXmM', 'lorem ipsum blabla', 3, 1),
 	(4, 'Morceau', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 4, 1),
-	(5, 'Lecture', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 1, 2),
+	(5, 'Lecture', NULL, 'https://www.youtube.com/embed/QYEj7U5CKzI', 'lorem ipsum blabla', 1, 2),
 	(6, 'Rythme', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 2, 2),
 	(7, 'Connaissances théoriques', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 3, 2),
 	(8, 'Morceau', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 4, 2);
@@ -91,7 +98,7 @@ CREATE TABLE IF NOT EXISTS `progression` (
   `id` int NOT NULL AUTO_INCREMENT,
   `utilisateur_id` int NOT NULL,
   `etape_id` int NOT NULL,
-  `done` tinyint(1) NOT NULL DEFAULT '0',
+  `done` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_D5B25073FB88E14F` (`utilisateur_id`),
   KEY `IDX_D5B250734A8CA2AD` (`etape_id`),
@@ -101,7 +108,7 @@ CREATE TABLE IF NOT EXISTS `progression` (
 
 -- Listage des données de la table projet_final_maximefutterer.progression : ~0 rows (environ)
 INSERT IGNORE INTO `progression` (`id`, `utilisateur_id`, `etape_id`, `done`) VALUES
-	(1, 1, 1, 0),
+	(1, 1, 1, 1),
 	(2, 1, 2, 1),
 	(3, 1, 3, 1),
 	(4, 1, 4, 1),
@@ -115,15 +122,16 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `email` varchar(180) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `roles` json NOT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `register_date` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet_final_maximefutterer.utilisateur : ~0 rows (environ)
+-- Listage des données de la table projet_final_maximefutterer.utilisateur : ~1 rows (environ)
 INSERT IGNORE INTO `utilisateur` (`id`, `email`, `roles`, `password`, `username`, `register_date`) VALUES
-	(1, 'futterermaxime@gmail.com', '[]', '$2y$13$2m0OgmrAHZyvSJ7e0A7m8e623cB4RmOhtQlROVH0DmBovNRjMqUiG', 'FutRR_', '2024-07-02 11:39:12');
+	(1, 'futterermaxime@gmail.com', '[]', '$2y$13$2m0OgmrAHZyvSJ7e0A7m8e623cB4RmOhtQlROVH0DmBovNRjMqUiG', 'FutRR_', '2024-07-02 11:39:12'),
+	(2, 'exemple@exemple.exemple', '[]', '$2y$13$.hS7yk5nrHkeBfPiiDDAVOXGDd4WCNAjC8saH.6dGR6KohqLhzJPu', 'exemple', '2024-07-05 14:33:10');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
