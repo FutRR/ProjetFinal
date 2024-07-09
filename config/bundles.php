3a6ca298fd75028afc12b118972943e6c67eea82
FutRR <futterermaxime@gmail.com>
2024-07-09 14:28:34 +0200

    added the REAL tinymce and removed the other

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Flasher\Symfony\FlasherSymfonyBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Eckinox\TinymceBundle\TinymceBundle::class => ['all' => true],
 ];
