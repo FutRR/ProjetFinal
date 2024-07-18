2009b6b1c10f4c9836a8cca9db24fcf55e435467
FutRR <futterermaxime@gmail.com>
2024-07-18 14:40:17 +0200

    started back from scratch due to bugs in stripe setup

--- migrations/Version20240718122308.php
+++ migrations/Version20240718122308.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240712074351 extends AbstractMigration
+final class Version20240718122308 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240712074351 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE avis CHANGE contenu contenu LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE abonnement ADD lien_paiement VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE avis CHANGE contenu contenu LONGTEXT NOT NULL');
+        $this->addSql('ALTER TABLE abonnement DROP lien_paiement');
     }
 }
