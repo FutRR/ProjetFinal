ab6f3300550c81b72655cced818d20e16739e0fd
FutRR <futterermaxime@gmail.com>
2024-09-19 00:20:35 +0200

    db export

--- db-export.sql
+++ db-export.sql
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `avis` (
   PRIMARY KEY (`id`),
   KEY `IDX_8F91ABF0FB88E14F` (`utilisateur_id`),
   CONSTRAINT `FK_8F91ABF0FB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table projet_final_maximefutterer.avis : ~16 rows (environ)
 REPLACE INTO `avis` (`id`, `utilisateur_id`, `contenu`, `date_creation`, `note`) VALUES
@@ -62,18 +62,34 @@ CREATE TABLE IF NOT EXISTS `etape` (
   PRIMARY KEY (`id`),
   KEY `IDX_285F75DDB3E9C81` (`niveau_id`),
   CONSTRAINT `FK_285F75DDB3E9C81` FOREIGN KEY (`niveau_id`) REFERENCES `niveau` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet_final_maximefutterer.etape : ~8 rows (environ)
+-- Listage des données de la table projet_final_maximefutterer.etape : ~12 rows (environ)
 REPLACE INTO `etape` (`id`, `nom_etape`, `pdf`, `video`, `description`, `ordre`, `niveau_id`) VALUES
-	(1, 'Lecture', 'pdf/Commandes-Symfony-6687b9c8a58dd.pdf', 'https://www.youtube.com/embed/n7lOJNYGYvw', 'lorem ipsum blabla', 1, 1),
-	(2, 'Rythme', 'pdf/piano-facile.pdf', 'https://www.youtube.com/embed/n7RjlO-beJA', 'lorem ipsum blabla', 2, 1),
+	(1, 'Lecture', 'pdf/au-clair-de-la-lune-66eae4d79bf15.pdf', 'https://www.youtube.com/embed/9UjAosN2CWc', 'lorem ipsum blabla', 1, 1),
+	(2, 'Rythme', 'pdf/piano-facile.pdf', 'https://www.youtube.com/embed/9UjAosN2CWc-beJA', 'lorem ipsum blabla', 2, 1),
 	(3, 'Théorie', 'pdf/Commandes-Symfony-6687b9c8a58dd.pdf', 'https://www.youtube.com/embed/YaGiM2cPXmM', 'lorem ipsum blabla', 3, 1),
-	(4, 'Morceau', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 4, 1),
+	(4, 'Morceau', 'pdf/piano-facile.pdf', 'https://www.youtube.com/embed/HjG5RxDIg-beJA', 'lorem ipsum blabla', 4, 1),
 	(5, 'Lecture', NULL, 'https://www.youtube.com/embed/QYEj7U5CKzI', 'lorem ipsum blabla', 1, 2),
 	(6, 'Rythme', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 2, 2),
 	(7, 'Théorie', 'pdf/piano-facile.pdf', 'https://www.youtube.com/embed/', 'lorem ipsum blabla', 3, 2),
-	(8, 'Morceau', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 4, 2);
+	(8, 'Morceau', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 4, 2),
+	(9, 'Lecture', 'pdf/au-clair-de-la-lune-66eae4d79bf15.pdf', 'https://www.youtube.com/embed/5WZkSaTBRCQ', 'lorem ipsum blabla', 1, 3),
+	(10, 'Rythme', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 2, 3),
+	(11, 'Théorie', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 3, 3),
+	(12, 'Morceau', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 4, 3),
+	(29, 'Lecture', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 1, 15),
+	(30, 'Rythme', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 2, 15),
+	(31, 'Théorie', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 3, 15),
+	(32, 'Morceau', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 4, 15),
+	(33, 'Lecture', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 1, 16),
+	(34, 'Rythme', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 2, 16),
+	(35, 'Théorie', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 3, 16),
+	(36, 'Morceau', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 4, 16),
+	(37, 'Lecture', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 1, 17),
+	(38, 'Rythme', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 2, 17),
+	(39, 'Théorie', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 3, 17),
+	(40, 'Morceau', 'pdf/piano-facile.pdf', NULL, 'lorem ipsum blabla', 4, 17);
 
 -- Listage de la structure de table projet_final_maximefutterer. messenger_messages
 CREATE TABLE IF NOT EXISTS `messenger_messages` (
@@ -100,12 +116,16 @@ CREATE TABLE IF NOT EXISTS `niveau` (
   `nom_niveau` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `prix` double DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet_final_maximefutterer.niveau : ~2 rows (environ)
+-- Listage des données de la table projet_final_maximefutterer.niveau : ~6 rows (environ)
 REPLACE INTO `niveau` (`id`, `nom_niveau`, `prix`) VALUES
 	(1, 'Niveau 1', NULL),
-	(2, 'Niveau 2', NULL);
+	(2, 'Niveau 2', NULL),
+	(3, 'Niveau 3', NULL),
+	(15, 'Niveau 4', NULL),
+	(16, 'Niveau 5', NULL),
+	(17, 'Niveau 6', NULL);
 
 -- Listage de la structure de table projet_final_maximefutterer. post
 CREATE TABLE IF NOT EXISTS `post` (
@@ -121,17 +141,26 @@ CREATE TABLE IF NOT EXISTS `post` (
   KEY `IDX_5A8A6C8D727ACA70` (`parent_id`),
   CONSTRAINT `FK_5A8A6C8D4A8CA2AD` FOREIGN KEY (`etape_id`) REFERENCES `etape` (`id`),
   CONSTRAINT `FK_5A8A6C8D727ACA70` FOREIGN KEY (`parent_id`) REFERENCES `post` (`id`),
-  CONSTRAINT `FK_5A8A6C8DFB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  CONSTRAINT `FK_5A8A6C8DFB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet_final_maximefutterer.post : ~6 rows (environ)
+-- Listage des données de la table projet_final_maximefutterer.post : ~14 rows (environ)
 REPLACE INTO `post` (`id`, `utilisateur_id`, `etape_id`, `contenu`, `date_creation`, `parent_id`) VALUES
 	(1, 1, 1, 'mon premier post :)', '2024-07-08 09:42:41', NULL),
 	(9, 2, 1, 'encore un test', '2024-07-09 08:15:19', NULL),
 	(11, 2, 1, 'et voila', '2024-07-09 12:02:32', 9),
 	(12, 2, 1, 'et voila', '2024-07-09 12:02:35', 9),
-	(13, 2, 1, 'yes', '2024-07-09 13:41:51', 1),
-	(20, NULL, 1, 'test', '2024-08-01 20:45:48', NULL);
+	(20, NULL, 1, 'test', '2024-08-01 20:45:48', NULL),
+	(21, 1, 6, 'Très bien! J\'ai pu apprendre ce morceau en quelques heures seulement!', '2024-08-29 12:07:47', NULL),
+	(22, 11, 3, 'Test', '2024-09-06 08:49:54', NULL),
+	(23, NULL, 3, 'test 1', '2024-09-06 09:13:57', NULL),
+	(24, NULL, 3, 'test 2', '2024-09-06 09:14:05', NULL),
+	(26, 11, 3, 'reponse', '2024-09-16 13:49:34', 23),
+	(27, 11, 3, 'test', '2024-09-16 13:49:55', 22),
+	(28, 11, 1, 'test home page', '2024-09-18 15:21:29', 12),
+	(29, NULL, 1, 'test home page 2', '2024-09-18 15:22:32', 28),
+	(30, 1, 1, 'test homepage 3', '2024-09-18 16:21:23', 1),
+	(31, 13, 39, 'Test 99', '2024-09-18 20:32:13', NULL);
 
 -- Listage de la structure de table projet_final_maximefutterer. progression
 CREATE TABLE IF NOT EXISTS `progression` (
@@ -144,9 +173,9 @@ CREATE TABLE IF NOT EXISTS `progression` (
   KEY `IDX_D5B250734A8CA2AD` (`etape_id`),
   CONSTRAINT `FK_D5B250734A8CA2AD` FOREIGN KEY (`etape_id`) REFERENCES `etape` (`id`),
   CONSTRAINT `FK_D5B25073FB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table projet_final_maximefutterer.progression : ~14 rows (environ)
+-- Listage des données de la table projet_final_maximefutterer.progression : ~18 rows (environ)
 REPLACE INTO `progression` (`id`, `utilisateur_id`, `etape_id`, `done`) VALUES
 	(1, 1, 1, 1),
 	(2, 1, 2, 1),
@@ -159,7 +188,19 @@ REPLACE INTO `progression` (`id`, `utilisateur_id`, `etape_id`, `done`) VALUES
 	(9, 2, 3, 0),
 	(10, 2, 2, 0),
 	(11, 3, 1, 0),
-	(12, 1, 8, 1);
+	(12, 1, 8, 1),
+	(16, 1, 9, 0),
+	(18, 11, 1, 1),
+	(19, 11, 2, 1),
+	(20, 11, 3, 1),
+	(21, 11, 4, 1),
+	(22, 11, 9, 0),
+	(26, 13, 39, 0),
+	(27, 13, 1, 0),
+	(28, 13, 2, 0),
+	(29, 13, 3, 0),
+	(30, 13, 4, 0),
+	(31, 1, 10, 0);
 
 -- Listage de la structure de table projet_final_maximefutterer. reset_password_request
 CREATE TABLE IF NOT EXISTS `reset_password_request` (
@@ -172,11 +213,9 @@ CREATE TABLE IF NOT EXISTS `reset_password_request` (
   PRIMARY KEY (`id`),
   KEY `IDX_7CE748AA76ED395` (`user_id`),
   CONSTRAINT `FK_7CE748AA76ED395` FOREIGN KEY (`user_id`) REFERENCES `utilisateur` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table projet_final_maximefutterer.reset_password_request : ~1 rows (environ)
-REPLACE INTO `reset_password_request` (`id`, `user_id`, `selector`, `hashed_token`, `requested_at`, `expires_at`) VALUES
-	(1, 1, 'HslUAHcarKuLXuaVi8Jb', '8l2rHQwXTbre41vGXRIKyUH9PHgQDyz4b1L/Gw5sKQs=', '2024-07-12 06:59:54', '2024-07-12 07:59:54');
 
 -- Listage de la structure de table projet_final_maximefutterer. utilisateur
 CREATE TABLE IF NOT EXISTS `utilisateur` (
@@ -189,12 +228,14 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `google_user` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table projet_final_maximefutterer.utilisateur : ~4 rows (environ)
 REPLACE INTO `utilisateur` (`id`, `email`, `roles`, `password`, `username`, `register_date`, `google_user`) VALUES
-	(1, 'admin@admin.com', '["ROLE_ADMIN"]', '$2y$13$UJkJ4XJcjkJji9kXxiKKWeajjb3eP1URQBF4Vl5pZA.WdP.XxufuG', 'FutRR_', '2024-07-02 11:39:12', 0),
-	(2, 'exemple@exemple.exemple', '["ROLE_USER"]', '$2y$13$.hS7yk5nrHkeBfPiiDDAVOXGDd4WCNAjC8saH.6dGR6KohqLhzJPu', 'exemple', '2024-07-05 14:33:10', 0);
+	(1, 'admin@maesterclass.com', '["ROLE_ADMIN"]', '$2y$13$UJkJ4XJcjkJji9kXxiKKWeajjb3eP1URQBF4Vl5pZA.WdP.XxufuG', 'FutRR', '2024-07-02 11:39:12', 0),
+	(2, 'exemple@exemple.com', '["ROLE_USER"]', '$2y$13$.hS7yk5nrHkeBfPiiDDAVOXGDd4WCNAjC8saH.6dGR6KohqLhzJPu', 'exemple', '2024-07-05 14:33:10', 0),
+	(11, 'maximefutterer68@gmail.com', '["ROLE_ADMIN"]', '6d9f60987b3d68f278072540c373bfa0', 'botspotter68', '2024-08-29 12:00:08', 1),
+	(13, 'futterermaxime@gmail.com', '["ROLE_USER"]', 'dbcd94bb941d1f5f4863bfefb6a9e9f8', 'futterermaxime', '2024-09-18 16:47:13', 1);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
