672fed18ef4178020f9f8e07db8e36045d6df8ac
FutRR <futterermaxime@gmail.com>
2024-09-18 23:04:48 +0200

    new pdf

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,23 +11,26 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $niveauId = 3;
-        $niveau = $manager->getRepository(Niveau::class)->find($niveauId);
-
         $nomEtapes = ['Lecture', 'Rythme', 'Théorie', 'Morceau'];
 
-        // Création d'étapes
-        foreach ($nomEtapes as $i => $nomEtape) {
-            $etape = new Etape();
-            $etape->setNomEtape($nomEtape);
-            $etape->setDescription('lorem ipsum blabla');
-            $etape->setPdf('pdf/piano-facile.pdf');
-            $etape->setOrdre($i + 1);
-            $etape->setNiveau($niveau);
-            $manager->persist($etape);
-        }
+        for ($niveauId = 1;$niveauId <= 3; $niveauId++){
+            $niveau = new Niveau();
+            $niveau->setNomNiveau('Niveau');
+            $manager->persist($niveau);
 
-        // Enregistrer les objets en base de données
-        $manager->flush();
+            // Création d'étapes
+            foreach ($nomEtapes as $i => $nomEtape) {
+                $etape = new Etape();
+                $etape->setNomEtape($nomEtape);
+                $etape->setDescription('lorem ipsum blabla');
+                $etape->setPdf('pdf/piano-facile.pdf');
+                $etape->setOrdre($i + 1);
+                $etape->setNiveau($niveau);
+                $manager->persist($etape);
+            }
+
+            // Enregistrer les objets en base de données
+            $manager->flush();
+        }
     }
 }
